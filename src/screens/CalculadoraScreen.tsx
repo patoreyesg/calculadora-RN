84b81d52cc2b0f79aff4ee49854e9c4d72c66eef
Patricio Reyes G <preyes@cydingenieria.cl>
2023-07-18 11:20:37 -0400

    pantalla principal de la calculadora

---
+++ src/screens/CalculadoraScreen.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import {styles} from '../themes/AppTheme';
+import {BotonCalc} from '../components/BotonCalc';
+
+export const CalculadoraScreen = () => {
+  return (
+    <View style={styles.calculadoraContainer}>
+      <Text style={styles.resultadoPequeno}>1.500,00</Text>
+      <Text style={styles.resultado}>1.500,00</Text>
+
+      <View style={styles.fila}>
+        <BotonCalc texto="C" color="#9B9B9B" />
+        <BotonCalc texto="+/-" color="#9B9B9B" />
+        <BotonCalc texto="del" color="#9B9B9B" />
+        <BotonCalc texto="/" color="#FF9427" />
+      </View>
+      {/* FILA DE BOTONES */}
+      <View style={styles.fila}>
+        <BotonCalc texto="7" />
+        <BotonCalc texto="8" />
+        <BotonCalc texto="9" />
+        <BotonCalc texto="x" color="#FF9427" />
+      </View>
+      {/* FILA DE BOTONES */}
+      <View style={styles.fila}>
+        <BotonCalc texto="4" />
+        <BotonCalc texto="5" />
+        <BotonCalc texto="6" />
+        <BotonCalc texto="-" color="#FF9427" />
+      </View>
+      {/* FILA DE BOTONES */}
+      <View style={styles.fila}>
+        <BotonCalc texto="1" />
+        <BotonCalc texto="2" />
+        <BotonCalc texto="3" />
+        <BotonCalc texto="+" color="#FF9427" />
+      </View>
+      {/* FILA DE BOTONES */}
+      <View style={styles.fila}>
+        <BotonCalc texto="0" ancho />
+        <BotonCalc texto="." />
+        <BotonCalc texto="=" color="#FF9427" />
+      </View>
+    </View>
+  );
+};
